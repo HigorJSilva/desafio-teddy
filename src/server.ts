0ac2845c82e81d036d3fe3e23faa107d9f155465
Higor Silva <higor.junio@hotmail.com>
2024-08-01 18:56:16 -0300

    feat: add typeorm data source

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
+import { AppDataSource } from '@shared/database/datasource';
 import application from '@shared/server';
 import dotenv from 'dotenv';
 import 'reflect-metadata';
 
 dotenv.config();
 
-application.listen(process.env.PORT, () => {
-  console.log('Server running');
+AppDataSource.initialize().then(() => {
+  console.log('Data Source running!');
+
+  application.listen(process.env.PORT, () => {
+    console.log('Server running');
+  });
 });
---
+++ src/shared/database/datasource.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT as unknown as number,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  synchronize: true,
+});
