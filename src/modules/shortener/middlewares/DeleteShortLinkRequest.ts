751521aa0ad38fc5cc11881da56bf0822d908e5e
Higor Silva <higor.junio@hotmail.com>
2024-08-02 11:40:01 -0300

    fix: unused imports

--- src/modules/shortener/middlewares/DeleteShortLinkRequest.ts
+++ src/modules/shortener/middlewares/DeleteShortLinkRequest.ts
@@ -1,5 +1,5 @@
-import { invalidUrl, requiredMessage } from '@shared/helpers/messages/messages';
-import { body, param } from 'express-validator';
+import { requiredMessage } from '@shared/helpers/messages/messages';
+import { param } from 'express-validator';
 
 export const DeleteShortLinkRequest = [
   param('id').notEmpty().withMessage(requiredMessage).isUUID(),
